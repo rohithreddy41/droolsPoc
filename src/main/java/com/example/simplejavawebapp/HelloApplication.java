ff6923f7aef704ff9be8b0331eed26c8c958550a
rohith janga <rohith.janga@usda.gov>
2022-01-07 05:22:36 -0700

    update

---
+++ src/main/java/com/example/simplejavawebapp/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.simplejavawebapp;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
